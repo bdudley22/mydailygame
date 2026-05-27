cc16cbab376e070e18d3f4cd2dc65f4d9c653eb6
Brooks <cchristopher.dudley@gmail.com>
2026-05-27 17:17:26 -0400

    full site

---
+++ game.js
@@ -0,0 +1,44 @@
+let button = document.querySelector("#gambleButton");
+let rouletteNumber = document.querySelector("#rouletteNumber");
+let dailyText = document.querySelector("#daily");
+button.addEventListener("click", playGamble);
+/* ---------------- DAILY NUMBER ---------------- */
+function getDailyNumber() {
+    let today = new Date().toISOString().split("T")[0];
+    let hash = 0;
+    for (let i = 0; i < today.length; i++) {
+        hash = today.charCodeAt(i) + ((hash << 5) - hash);
+    }
+    return Math.abs(hash) % 1000;
+}
+dailyText.textContent = "daily number is: " + getDailyNumber();
+
+/* ---------------- GAMBLE LOGIC ---------------- */
+function playGamble(e){
+    e.preventDefault();
+    const sound = new Audio("sounds/csgo.mp3");
+    sound.play();
+    let finalNumber;
+    let winChance = Math.floor(Math.random() * 100) + 1;
+    if(winChance === 100){
+        finalNumber = 777;
+    } else {
+        finalNumber = Math.floor(Math.random() * 999);
+    }
+    setTimeout(() => {
+        let counter = 0;
+        let delay = 40;
+        function spin() {
+            rouletteNumber.textContent =
+                Math.floor(Math.random() * 999);
+            counter++;
+            delay += 2;
+            if(counter < 60){
+                setTimeout(spin, delay);
+            } else {
+                rouletteNumber.textContent = finalNumber;
+            }
+        }
+        spin();
+    }, 1500);
+}
