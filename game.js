ae6b6b5cf1a0cdf7e6814d75aea8dac673b722f7
Brooks <cchristopher.dudley@gmail.com>
2026-05-27 17:34:46 -0400

    added winning

--- game.js
+++ game.js
@@ -3,6 +3,15 @@ let rouletteNumber = document.querySelector("#rouletteNumber");
 let dailyText = document.querySelector("#daily");
 let isSpinning = false;
 button.addEventListener("click", playGamble);
+function triggerWinEffect() {
+    const winSound = new Audio("sounds/wow.mp3");
+    winSound.play();
+    document.body.style.transition = "background-color 0.2s ease";
+    document.body.style.backgroundColor = "#39ff14";
+    setTimeout(() => {
+        document.body.style.backgroundColor = "#000000";
+    }, 3000);
+}
 /* ---------------- DAILY NUMBER ---------------- */
 function getDailyNumber() {
     let today = new Date().toISOString().split("T")[0];
@@ -23,8 +32,9 @@ function playGamble(e){
     sound.play();
     let finalNumber;
     let winChance = Math.floor(Math.random() * 100) + 1;
+    let daily = getDailyNumber();
     if(winChance === 100){
-        finalNumber = 777;
+        finalNumber = daily;
     } else {
         finalNumber = Math.floor(Math.random() * 999);
     }
@@ -40,6 +50,10 @@ function playGamble(e){
                 setTimeout(spin, delay);
             } else {
                 rouletteNumber.textContent = finalNumber;
+                let daily = getDailyNumber();
+                if (finalNumber === daily) {
+                    triggerWinEffect();
+                }
                 isSpinning = false;
             }
         }
